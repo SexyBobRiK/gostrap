63e7fedb7b7199bfc3b82dbd932aee533601b0ea
SexyBobRiK <bagpuma8@gmail.com>
2026-02-07 00:07:04 +0500

    fix: Gin ProviderInit -> invalid memory address or nil pointer dereference

--- provider/gin_provider.go
+++ provider/gin_provider.go
@@ -2,8 +2,8 @@ package provider
 
 import (
 	"github.com/SexyBobRiK/gostrap/config"
-
 	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,20 +17,22 @@ func (GinProvider) ProviderInit(cfgGin config.GinEntity) (*gin.Engine, error) {
 			gin.SetMode(gin.DebugMode)
 		}
 		ginEngine := gin.Default()
-		if cfgGin.Middleware.Cors.Enabled {
-			ginEngine.Use(cors.New(cors.Config{
-				AllowCredentials: cfgGin.Middleware.Cors.AllowCredentials,
-				AllowOrigins:     cfgGin.Middleware.Cors.AllowOrigins,
-				AllowMethods:     cfgGin.Middleware.Cors.AllowMethods,
-				AllowHeaders:     cfgGin.Middleware.Cors.AllowHeaders,
-				ExposeHeaders:    cfgGin.Middleware.Cors.ExposeHeaders,
-			}))
-			if cfgGin.Middleware.StaticFile.Enable {
-				ginEngine.Static(cfgGin.Middleware.StaticFile.UrlOne, cfgGin.Middleware.StaticFile.UrlTwo)
-				ginEngine.MaxMultipartMemory = cfgGin.Middleware.StaticFile.MaxMultipartMemory
+		if cfgGin.Middleware != nil {
+			if cfgGin.Middleware.Cors.Enabled && cfgGin.Middleware.Cors != nil {
+				ginEngine.Use(cors.New(cors.Config{
+					AllowCredentials: cfgGin.Middleware.Cors.AllowCredentials,
+					AllowOrigins:     cfgGin.Middleware.Cors.AllowOrigins,
+					AllowMethods:     cfgGin.Middleware.Cors.AllowMethods,
+					AllowHeaders:     cfgGin.Middleware.Cors.AllowHeaders,
+					ExposeHeaders:    cfgGin.Middleware.Cors.ExposeHeaders,
+				}))
+				if cfgGin.Middleware.StaticFile.Enable && cfgGin.Middleware.StaticFile != nil {
+					ginEngine.Static(cfgGin.Middleware.StaticFile.UrlOne, cfgGin.Middleware.StaticFile.UrlTwo)
+					ginEngine.MaxMultipartMemory = cfgGin.Middleware.StaticFile.MaxMultipartMemory
+				}
+			} else {
+				ginEngine.Use(cors.Default())
 			}
-		} else {
-			ginEngine.Use(cors.Default())
 		}
 		return ginEngine, nil
 	}
